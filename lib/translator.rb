1ff65e4c716a4d36e57843c3d7479175dd407002
Jacob Squier <jacob.squier@gmail.com>
2020-08-18 03:03:58 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,14 @@
-# require modules here
+require "yaml"
+require "pry"
 
-def load_library
-  # code goes here
+def load_library(file)
+
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file, emoticon)
+
 end
 
-def get_english_meaning
+def get_english_meaning(emoticon)
   # code goes here
 end
